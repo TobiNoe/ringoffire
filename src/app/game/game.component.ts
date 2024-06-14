cbabe11a9fee941e2cb59e1e9c848a8662bf4d4d
Tobias Nölle <tobias.noelle@icloud.com>
2024-06-14 20:18:46 +0200

    style: top-card by hover and click

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false;
+
+  takeCard() {
+      this.pickCardAnimation = true;
+  }
 
 }
