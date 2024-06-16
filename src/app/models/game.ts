d19371da0168e1d1fc66bfb9235f4d76c694a56c
Tobias Nölle <tobias.noelle@icloud.com>
2024-06-16 19:59:43 +0200

    feat: added for loop to render all players from game.players[]

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Melanie', 'Moritz', 'Tobias'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
