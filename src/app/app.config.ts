03831bbf35a5e3e2ea203cdb0102963414f95bfc
Tobias Nölle <tobias.noelle@icloud.com>
2024-07-03 19:47:56 +0200

    update: added Angular/Fire in project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ringoffire-2caba","appId":"1:812402739456:web:526f366eceee691e614be5","storageBucket":"ringoffire-2caba.appspot.com","apiKey":"AIzaSyCpqMm9yIL3oPP86FzJ1RCkkXrUhCz3KhU","authDomain":"ringoffire-2caba.firebaseapp.com","messagingSenderId":"812402739456"})), provideFirestore(() => getFirestore())]
 };
